450de886f91881ca9ba396795dc524f6f837c0ac
Lola <lola_chacon8@hotmail.com>
2025-06-18 13:00:08 +0200

    instalacion express, nodemon para preparar el proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const app= express();
+require('dotenv').config();
+
+app.get('/',(req,res)=>{
+    res.send('hola servidor');
+});
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT,()=>{
+    console.log(`Servidor escuchando en puerto ${PORT}`);
+});
