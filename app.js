6e86d8d5c9c8e8d18ff1cbd97e23db38aa03844a
Lola <lola_chacon8@hotmail.com>
2025-06-19 11:25:46 +0200

    importacion libreria cors y mongoose

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require ('express');
 const app= express();
-require('dotenv').config();
+const cors = require('cors');
 
-app.get('/',(req,res)=>{
-    res.send('hola servidor');
-});
+
+require('dotenv').config();
+require('./db.js');
+
+const apiRouter= require('./routes/api.js');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/api', apiRouter);
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT,()=>{
