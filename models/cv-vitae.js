0beb89af180b4a4c9c3a491a4a852eb9b5591dc7
Lola <lola_chacon8@hotmail.com>
2025-06-22 18:45:56 +0200

    ajustes model cv-vitae

--- models/cv-vitae.js
+++ models/cv-vitae.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let cv = new Schema ({
+    id:Number, 
     title:String,
-    dates: Date,
-    datesEnd: Date, 
+    function:String, 
+    dates: String,
+    datesEnd: String, 
     place: String,
     skill: [String],
     image: String,
+    type:String
 
-})
+}
+
+
+)
 
 module.exports = mongoose.model('cv',cv);
